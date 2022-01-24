bee4962769e6966cb6847cce0d4851ea717d4a64
Min Ding <dingmin@alum.mit.edu>
2022-01-24 12:04:14 +0800

    Add files via upload

---
+++ Fig4b_Tg30_Th4/PlotResult.m
@@ -0,0 +1,254 @@
+%% load start and end positions of crustal nodes
+
+filename = 'T15_Start.rpt';
+startRow = 14;
+endRow = 11107;
+formatSpec = '%*16s%16f%f%[^\n\r]';
+fileID = fopen(filename,'r');
+textscan(fileID, '%[^\n\r]', startRow-1, 'WhiteSpace', '', 'ReturnOnError', false);
+dataArray = textscan(fileID, formatSpec, endRow-startRow+1, 'Delimiter', '', 'WhiteSpace', '', 'TextType', 'string', 'EmptyValue', NaN, 'ReturnOnError', false, 'EndOfLine', '\r\n');
+
+fclose(fileID);
+
+X0 = dataArray{:, 1};
+Y0 = dataArray{:, 2};
+
+clearvars filename formatSpec fileID dataArray ans;
+
+
+filename = 'T540_End.rpt';
+
+
+formatSpec = '%*16s%16f%f%[^\n\r]';
+
+fileID = fopen(filename,'r');
+
+textscan(fileID, '%[^\n\r]', startRow-1, 'WhiteSpace', '', 'ReturnOnError', false);
+dataArray = textscan(fileID, formatSpec, endRow-startRow+1, 'Delimiter', '', 'WhiteSpace', '', 'TextType', 'string', 'EmptyValue', NaN, 'ReturnOnError', false, 'EndOfLine', '\r\n');
+
+fclose(fileID);
+X = dataArray{:, 1};
+Y = dataArray{:, 2};
+
+clearvars filename startRow endRow formatSpec fileID dataArray ans;
+
+X = X*1e-3;
+Y = Y*1e-3;
+X0 = X0*1e-3;
+Y0 = Y0*1e-3;
+%% load heat flux of reference surface
+filename = 'HeatFlux.rpt';
+startRow = 2;
+endRow = 1084;
+
+formatSpec = '%30s%s%[^\n\r]';
+
+fileID = fopen(filename,'r');
+
+textscan(fileID, '%[^\n\r]', startRow-1, 'WhiteSpace', '', 'ReturnOnError', false);
+dataArray = textscan(fileID, formatSpec, endRow-startRow+1, 'Delimiter', '', 'WhiteSpace', '', 'TextType', 'string', 'ReturnOnError', false, 'EndOfLine', '\r\n');
+
+fclose(fileID);
+raw = repmat({''},length(dataArray{1}),length(dataArray)-1);
+for col=1:length(dataArray)-1
+    raw(1:length(dataArray{col}),col) = mat2cell(dataArray{col}, ones(length(dataArray{col}), 1));
+end
+numericData = NaN(size(dataArray{1},1),size(dataArray,2));
+
+for col=[1,2]
+    % 将输入元胞数组中的文本转换为数值。已将非数值文本替换为 NaN。
+    rawData = dataArray{col};
+    for row=1:size(rawData, 1)
+        % 创建正则表达式以检测并删除非数值前缀和后缀。
+        regexstr = '(?<prefix>.*?)(?<numbers>([-]*(\d+[\,]*)+[\.]{0,1}\d*[eEdD]{0,1}[-+]*\d*[i]{0,1})|([-]*(\d+[\,]*)*[\.]{1,1}\d+[eEdD]{0,1}[-+]*\d*[i]{0,1}))(?<suffix>.*)';
+        try
+            result = regexp(rawData(row), regexstr, 'names');
+            numbers = result.numbers;
+            
+            % 在非千位位置中检测到逗号。
+            invalidThousandsSeparator = false;
+            if numbers.contains(',')
+                thousandsRegExp = '^[-/+]*\d+?(\,\d{3})*\.{0,1}\d*$';
+                if isempty(regexp(numbers, thousandsRegExp, 'once'))
+                    numbers = NaN;
+                    invalidThousandsSeparator = true;
+                end
+            end
+            % 将数值文本转换为数值。
+            if ~invalidThousandsSeparator
+                numbers = textscan(char(strrep(numbers, ',', '')), '%f');
+                numericData(row, col) = numbers{1};
+                raw{row, col} = numbers{1};
+            end
+        catch
+            raw{row, col} = rawData{row};
+        end
+    end
+end
+
+
+R = cellfun(@(x) ~isnumeric(x) && ~islogical(x),raw); % 查找非数值元胞
+raw(R) = {NaN}; % 替换非数值元胞
+
+heatflux = cell2mat(raw);
+
+clearvars filename formatSpec fileID dataArray ans raw col numericData rawData row regexstr result numbers invalidThousandsSeparator thousandsRegExp R;
+
+%% load annulus temperature
+filename = 'AnnulusTemp.rpt';
+
+formatSpec = '%30s%s%[^\n\r]';
+
+fileID = fopen(filename,'r');
+
+textscan(fileID, '%[^\n\r]', startRow-1, 'WhiteSpace', '', 'ReturnOnError', false);
+dataArray = textscan(fileID, formatSpec, endRow-startRow+1, 'Delimiter', '', 'WhiteSpace', '', 'TextType', 'string', 'ReturnOnError', false, 'EndOfLine', '\r\n');
+
+fclose(fileID);
+raw = repmat({''},length(dataArray{1}),length(dataArray)-1);
+for col=1:length(dataArray)-1
+    raw(1:length(dataArray{col}),col) = mat2cell(dataArray{col}, ones(length(dataArray{col}), 1));
+end
+numericData = NaN(size(dataArray{1},1),size(dataArray,2));
+
+for col=[1,2]
+    % 将输入元胞数组中的文本转换为数值。已将非数值文本替换为 NaN。
+    rawData = dataArray{col};
+    for row=1:size(rawData, 1)
+        % 创建正则表达式以检测并删除非数值前缀和后缀。
+        regexstr = '(?<prefix>.*?)(?<numbers>([-]*(\d+[\,]*)+[\.]{0,1}\d*[eEdD]{0,1}[-+]*\d*[i]{0,1})|([-]*(\d+[\,]*)*[\.]{1,1}\d+[eEdD]{0,1}[-+]*\d*[i]{0,1}))(?<suffix>.*)';
+        try
+            result = regexp(rawData(row), regexstr, 'names');
+            numbers = result.numbers;
+            
+            % 在非千位位置中检测到逗号。
+            invalidThousandsSeparator = false;
+            if numbers.contains(',')
+                thousandsRegExp = '^[-/+]*\d+?(\,\d{3})*\.{0,1}\d*$';
+                if isempty(regexp(numbers, thousandsRegExp, 'once'))
+                    numbers = NaN;
+                    invalidThousandsSeparator = true;
+                end
+            end
+            % 将数值文本转换为数值。
+            if ~invalidThousandsSeparator
+                numbers = textscan(char(strrep(numbers, ',', '')), '%f');
+                numericData(row, col) = numbers{1};
+                raw{row, col} = numbers{1};
+            end
+        catch
+            raw{row, col} = rawData{row};
+        end
+    end
+end
+
+
+R = cellfun(@(x) ~isnumeric(x) && ~islogical(x),raw); % 查找非数值元胞
+raw(R) = {NaN}; % 替换非数值元胞
+
+Ta = cell2mat(raw);
+
+clearvars filename formatSpec fileID dataArray ans raw col numericData rawData row regexstr result numbers invalidThousandsSeparator thousandsRegExp R;
+
+%% load annulus vertical displacement
+filename = 'AnnulusVerDisp.rpt';
+
+
+formatSpec = '%30s%s%[^\n\r]';
+
+fileID = fopen(filename,'r');
+
+textscan(fileID, '%[^\n\r]', startRow-1, 'WhiteSpace', '', 'ReturnOnError', false);
+dataArray = textscan(fileID, formatSpec, endRow-startRow+1, 'Delimiter', '', 'WhiteSpace', '', 'TextType', 'string', 'ReturnOnError', false, 'EndOfLine', '\r\n');
+
+fclose(fileID);
+raw = repmat({''},length(dataArray{1}),length(dataArray)-1);
+for col=1:length(dataArray)-1
+    raw(1:length(dataArray{col}),col) = mat2cell(dataArray{col}, ones(length(dataArray{col}), 1));
+end
+numericData = NaN(size(dataArray{1},1),size(dataArray,2));
+
+for col=[1,2]
+    % 将输入元胞数组中的文本转换为数值。已将非数值文本替换为 NaN。
+    rawData = dataArray{col};
+    for row=1:size(rawData, 1)
+        % 创建正则表达式以检测并删除非数值前缀和后缀。
+        regexstr = '(?<prefix>.*?)(?<numbers>([-]*(\d+[\,]*)+[\.]{0,1}\d*[eEdD]{0,1}[-+]*\d*[i]{0,1})|([-]*(\d+[\,]*)*[\.]{1,1}\d+[eEdD]{0,1}[-+]*\d*[i]{0,1}))(?<suffix>.*)';
+        try
+            result = regexp(rawData(row), regexstr, 'names');
+            numbers = result.numbers;
+            
+            % 在非千位位置中检测到逗号。
+            invalidThousandsSeparator = false;
+            if numbers.contains(',')
+                thousandsRegExp = '^[-/+]*\d+?(\,\d{3})*\.{0,1}\d*$';
+                if isempty(regexp(numbers, thousandsRegExp, 'once'))
+                    numbers = NaN;
+                    invalidThousandsSeparator = true;
+                end
+            end
+            % 将数值文本转换为数值。
+            if ~invalidThousandsSeparator
+                numbers = textscan(char(strrep(numbers, ',', '')), '%f');
+                numericData(row, col) = numbers{1};
+                raw{row, col} = numbers{1};
+            end
+        catch
+            raw{row, col} = rawData{row};
+        end
+    end
+end
+
+
+R = cellfun(@(x) ~isnumeric(x) && ~islogical(x),raw); % 查找非数值元胞
+raw(R) = {NaN}; % 替换非数值元胞
+
+Dz = cell2mat(raw);
+
+clearvars filename startRow endRow formatSpec fileID dataArray ans raw col numericData rawData row regexstr result numbers invalidThousandsSeparator thousandsRegExp R;
+
+%%
+figure;
+
+subplot(4,1,1)
+
+[xx,yy]=meshgrid(0:1:582,-60:1:-10);
+f = scatteredInterpolant(X,Y,X-X0);
+Grid_Dx = f(xx,yy);
+
+pcolor(xx,yy,Grid_Dx);
+shading flat
+load coulomb3.0_colormap.mat
+colormap(stress_color)
+caxis([-30 30])
+hold on;
+
+k = boundary(X0,Y0,1);
+plot(X0(k),Y0(k),'k--')
+
+k = boundary(X,Y,1);
+plot(X(k),Y(k),'k')
+
+xlim([0 582])
+ylim([-50 -5])
+
+subplot(4,1,2)
+plot(heatflux(:,1)/3e13,heatflux(:,2)*1e3,'k');
+xlabel 'Time of evolution (Ma)'
+ylabel 'Heat flux (mW/m^2)'
+xlim([0 130])
+
+subplot(4,1,3)
+plot(Ta(:,1)/3e13,Ta(:,2),'k');
+xlabel 'Time of evolution (Ma)'
+ylabel 'Annulus Temp (K)'
+xlim([0 130])
+
+subplot(4,1,4)
+
+s = Dz(:,1)>0.001*3e13;
+plot(Dz(s,1)/3e13,(Dz(s,2)*1e-3+41.97),'k');
+xlabel 'Time of evolution (Ma)'
+ylabel 'Mean depth of annulus materials (km)'
+xlim([0 130])
+
